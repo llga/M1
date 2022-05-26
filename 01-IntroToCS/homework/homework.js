5cbc9b9daefa42736ce0291d0adf61174295fe19
Jose Lopez <josefrancisco2697@gmail.com>
2022-05-26 12:10:35 -0500

    resolved homework 00

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,22 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let decimal = 0
+  for (let i = 0; i < num.length; i++) {
+    decimal += num[i] * 2 ** (num.length - 1 - i);
+  }
+  return decimal
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let number = []
 
+  while (num > 0) {
+    number.unshift(num % 2)
+    num = Math.floor(num / 2)
+  }
+  return number.join("")
 }
 
 
