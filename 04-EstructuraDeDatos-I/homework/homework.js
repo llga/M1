c5c1e6276c22857273f0b84b077f6ce88e19d1ec
Jose Lopez <josefrancisco2697@gmail.com>
2022-05-26 18:51:19 -0500

    resolved homework 03

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -8,16 +8,21 @@ nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número 
 nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
 Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
 
-Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
+Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 
 
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
 function nFactorial(n) {
-}
+    if (Math.sign(n) === -1) throw "Se necesita un número entrero mayor a 1"
+    if (n < 2) return 1
+    return n * nFactorial(n - 1)
+  }
 
 function nFibonacci(n) {
+    if (n < 2) return n
+    return nFibonacci(n - 2) + nFibonacci(n - 1)
 }
 
 /*
@@ -30,7 +35,17 @@ Pueden utilizar class o función constructora.
 */
 
 function Queue() {
-
+  this.arr = []
+}
+Queue.prototype.enqueue = function(data) {
+  return this.arr.push(data)
+}
+Queue.prototype.dequeue = function(){
+  if(!this.arr) return undefined
+  return this.arr.shift()
+}
+Queue.prototype.size = function(){
+  return this.arr.length
 }
 
 // No modifiquen nada debajo de esta linea
